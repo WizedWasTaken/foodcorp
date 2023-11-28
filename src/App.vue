c0477fbcd02744eb4b1903aa6e560198916d1612
WizedWasTaken <jegerfattig1@gmail.com>
2023-11-28 15:29:11 +0100

    SideBar Added

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
-  <NavBar />
-  <router-view />
-  <FooterBar />
+  <div id="papaErAdem">
+    <SideBar />
+    <div id="notSidebar">
+      <NavBar />
+      <router-view />
+      <FooterBar />
+    </div>
+  </div>
 </template>
 
 <!-- GLOBAL SCSS -->
@@ -11,16 +16,27 @@ body {
   padding: 0;
   font-family: "Roboto", sans-serif;
 }
+
+#papaErAdem {
+  display: flex;
+}
+
+#notSidebar {
+  width: calc(100vw - 250px);
+  flex-direction: column;
+}
 </style>
 
 <script>
 import NavBar from "@/components/NavBar.vue";
 import FooterBar from "@/components/FooterBar.vue";
+import SideBar from "@/components/SideBar.vue";
 
 export default {
   components: {
     NavBar,
     FooterBar,
+    SideBar,
   },
 };
 </script>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="sidebar"></div>
+</template>
+
+<script></script>
+
+<style scoped>
+.sidebar {
+  width: 250px;
+  height: 100vh;
+  background-color: #f5f5f5;
+  padding: 20px;
+  box-sizing: border-box;
+  left: 0;
+  border-right: 2px solid black;
+}
+</style>

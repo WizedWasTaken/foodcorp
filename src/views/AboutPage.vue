423fa58f67f2925226000b00650fecc04341339b
WizedWasTaken <jegerfattig1@gmail.com>
2023-11-28 15:23:49 +0100

    Missing templates

---
+++ src/views/AboutPage.vue
@@ -0,0 +1,12 @@
+<!-- HTML -->
+<template>
+  <div>
+    <h1>About</h1>
+  </div>
+</template>
+
+<!-- SCSS -->
+<style scoped lang="scss"></style>
+
+<!-- JavaScript - NIKS PILLE! -->
+<script></script>
---
+++ src/views/MenuView.vue
@@ -0,0 +1,12 @@
+<!-- HTML -->
+<template>
+  <div>
+    <h1>Menu Kort</h1>
+  </div>
+</template>
+
+<!-- SCSS -->
+<style scoped lang="scss"></style>
+
+<!-- JavaScript - NIKS PILLE! -->
+<script></script>
